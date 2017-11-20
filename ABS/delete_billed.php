348d166003c69732167bb74d2b1f6d3f073960b0
Vyshu123 <33722092+Vyshu123@users.noreply.github.com>
2017-11-20 15:39:55 +0530

    billing system

---
+++ ABS/delete_billed.php
@@ -0,0 +1,13 @@
+<?php
+include 'dbCon.php';
+?>
+<?php
+	$del =$_REQUEST['id'];
+
+	mysql_query("DELETE FROM bill WHERE bill_id = '$del'")or die(mysql_error());  	
+	
+?>
+<script>
+alert('Deleted Successfuly');
+window.location = "billed.php";
+</script>
